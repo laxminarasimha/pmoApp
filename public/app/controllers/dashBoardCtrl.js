ee84a5f49bc85d4b2972e2507ae654819ddfd405
Laxmi <laxminarasimha.mylavarapu@capgemini.com>
2017-07-12 15:44:02 +0530

    pi chart template added to dash board

--- public/app/controllers/dashBoardCtrl.js
+++ public/app/controllers/dashBoardCtrl.js
@@ -21,6 +21,8 @@ Controller.$inject = ['$scope', '$rootScope','$filter','dashboardService','resou
      getProjectGraph(dashboardService,app,$filter);     
      getProjectChartjs(dashboardService,app,$filter,ctx);     
      getResourceData(resourceService,$scope);
+      var piChart = document.getElementById('piChart').getContext('2d');
+     createPieChart(dashboardService,$scope,$filter,piChart);
  }
 
  function getResourceData(resourceService,$scope){
@@ -32,8 +34,6 @@ Controller.$inject = ['$scope', '$rootScope','$filter','dashboardService','resou
  }
 
 function getProjectChartjs(dashboardService,$scope,$filter,ctx){
-
-   
 var chart = new Chart(ctx, {
     type: 'bar',
     data: {
@@ -71,6 +71,42 @@ var chart = new Chart(ctx, {
     }
 });
 
+}//End of function getProjectChartjs()
+
+function createPieChart(dashboardService,$scope,$filter,ctx){
+    var chart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        labels: ["Jan-17", "Feb-17", "Mar-17", "Apr-17", "May-17", "Jun-17"],
+        datasets: [{
+            label: '# of Votes',
+            data: [2478,5267,734,784,433],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'Sufficeient Sweden'
+      }
+    }
+});
 }
  
  function getProjectGraph(dashboardService,$scope,$filter){        
