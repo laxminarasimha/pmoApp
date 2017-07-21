f2a6c5d2fd7ba32666dcd5ef452f7ed79b84f215
Rishi Kumar Gupta <rishi.gupta@capgemini.com>
2017-07-21 19:12:33 +0530

    profile pic chnages

--- public/app/controllers/profileCtrls.js
+++ public/app/controllers/profileCtrls.js
@@ -34,7 +34,7 @@
             data:{file:file} //pass file as data, should be user ng-model
         }).then(function (resp) { //upload function returns a promise
             if(resp.data.error_code === 0){ //validate success
-                $window.alert('Success ' + resp.config.data.file.name + 'uploaded. Response: ');
+                //$window.alert('Success ' + resp.config.data.file.name + 'uploaded. Response: ');
             } else {
                 $window.alert('an error occured');
             }
@@ -42,9 +42,9 @@
             console.log('Error status: ' + resp.status);
             $window.alert('Error status: ' + resp.status);
         }, function (evt) { 
-            console.log(evt);
+            //console.log(evt);
             var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
-            console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
+            //console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
             vm.progress = 'progress: ' + progressPercentage + '% '; // capture upload progress
         });
     };
