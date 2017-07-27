bc4574919fd61621669972058a5f1c36b0bdfff9
Rishi Kumar Gupta <rishi.gupta@capgemini.com>
2017-07-27 15:12:54 +0530

    customized menu

--- public/app/controllers/menuCtrl.js
+++ public/app/controllers/menuCtrl.js
@@ -10,7 +10,32 @@
  
  $http.get("app/data/menuData.txt").then(function(response) {
               $scope.menuData = response.data;
+              //getCustomizedmenu($scope,Auth,response.data);
           });
+ }
+
+ function getCustomizedmenu($scope,Auth,menu){
+    var menuDataArray = [];
+ 	if(Auth.isLoggedIn()){		
+		Auth.getUser().then(function(data){										
+		if(data.data.resourcename !== ""){			
+			var userType = data.data.etype;
+			for(var i=0;i<menu.length;i++){
+				for(var j=0;j<menu[i].role.length;j++){
+                   if(userType == menu[i].role[j]){
+                      menuDataArray.push(menu[i]);
+                      break;
+                   }
+				}
+                
+			}
+		}else{
+			console.log("User is not found");				
+		}
+
+	});
+	}
+	$scope.menuData = menuDataArray;
  }
  
  })();
