1d87fe95ee3e83b53ed5250d3165e31fb38eee1c
Laxmi <laxminarasimha.mylavarapu@capgemini.com>
2017-08-04 15:41:48 +0530

    buttons added for role

--- public/app/controllers/roleCtrl.js
+++ public/app/controllers/roleCtrl.js
@@ -109,6 +109,9 @@ $scope.editRole = function (id) {
         $scope.vm = {};
         $scope.vm.dtInstance = null;  
         $scope.vm.dtOptions = DTOptionsBuilder.newOptions().withOption('order', [0, 'asc']);
+        $scope.vm.dtOptions.withDOM('Bfrtip');
+        $scope.vm.dtOptions.withOption('buttons',['copy', 'print', 'pdf','excel']);
+
          
 //=============================================================//
  }
