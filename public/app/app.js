b67d0fd480eef4adcc6fe13d9904e062273f1b74
sudhakar <sudhakar.vaka@capgemini.com>
2018-07-25 11:21:53 +0530

    changes

--- public/app/app.js
+++ public/app/app.js
@@ -7,12 +7,12 @@ angular.module('pmoApp', ['appRoutes', 'userController', 'userServices', 'ngAnim
         $httpProvider.defaults.headers.common["Cache-Control"] = "no-cache";
         $httpProvider.defaults.headers.common.Pragma = "no-cache";
         $httpProvider.defaults.headers.common["If-Modified-Since"] = "0";
-        
+
         $httpProvider.interceptors.push('AuthInterceptors');
     }).controller('footerCtrl', ['$scope', 'appConstants', function ($scope, appConstants) {
         $scope.rightToReserveMsg = appConstants.right_reserve;
 
     }]).constant("globalConfig", {
-        //apiAddress: 'http://din66008608.corp.capgemini.com:3000/api'
-        apiAddress: 'http://localhost:3000/api'
+        apiAddress: 'http://din66008608.corp.capgemini.com:3000/api'
+        //apiAddress: 'http://localhost:3000/api'
     });;  
